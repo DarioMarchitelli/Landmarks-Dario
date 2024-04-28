092434ab4e36df5de632ce3cb4eb24dc1523b8bd
DarioMarchitelli <d.marchitelli1@studenti.poliba.it>
2024-04-28 22:08:00 +0200

    Initial Commit

---
+++ Landmarks-Dario/Landmarks_DarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_DarioApp.swift
+//  Landmarks-Dario
+//
+//  Created by Dario Marchitelli on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_DarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
